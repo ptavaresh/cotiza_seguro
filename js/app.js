7255883751896b94b318b37eecb3c7873312d3d6
Pedro Tavares <ptavaresh@gmail.com>
2019-04-03 23:16:58 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Cotizador constructor
+
+function Seguro() {
+
+}
+
+const max = new Date().getFullYear(),
+      min = max - 20;
+
+const selectAnios = document.getElementById('anio');
+for (let i = max; i > min; i--) {
+      let option = document.createElement('option');
+      option.value = i;
+      option.innerHTML = i;
+      selectAnios.appendChild(option);
+      console.log(option)
+}
